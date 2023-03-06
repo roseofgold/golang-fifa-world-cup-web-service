a2f4a597f5c60dd794c8bb7fb4bed6ff44cfe301
Melinda Serven <melinda.serven@acquia.com>
2023-03-06 15:14:42 -0800

    Return filtered results

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -21,6 +21,13 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 			return
 		}
 		res.Write(winners)
+	} else {
+		filteredWinners, err := data.ListAllByYear(year)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		res.Write(filteredWinners)
 	}
 }
 
