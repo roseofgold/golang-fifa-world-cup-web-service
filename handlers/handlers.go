7099e5b61292999b6adfbf49e96362fc85e4d304
Melinda Serven <melinda.serven@acquia.com>
2023-03-06 15:29:30 -0800

    respond to invalid methods

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -54,5 +54,7 @@ func WinnersHandler(res http.ResponseWriter, req *http.Request) {
 		ListWinners(res, req)
 	case http.MethodPost:
 		AddNewWinner(res, req)
+	default:
+		res.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
